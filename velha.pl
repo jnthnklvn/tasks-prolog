51656a08d03fc783a0d876e403684a2e4ddfe25d
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2018-02-03 22:23:05 -0200

    Funcionando (falta preparar pra integração)

---
+++ velha.pl
@@ -0,0 +1,70 @@
+vitoria([XY,XY,XY,_,_,_,_,_,_],XY).
+vitoria([_,_,_,XY,XY,XY,_,_,_],XY).
+vitoria([_,_,_,_,_,_,XY,XY,XY],XY).
+vitoria([XY,_,_,XY,_,_,XY,_,_],XY).
+vitoria([_,XY,_,_,XY,_,_,XY,_],XY).
+vitoria([_,_,XY,_,_,XY,_,_,XY],XY).
+vitoria([XY,_,_,_,XY,_,_,_,XY],XY).
+vitoria([_,_,XY,_,XY,_,XY,_,_],XY).
+
+moverIA([A,B,C,D,a,F,G,H,I], XY, [A,B,C,D,XY,F,G,H,I]).
+moverIA([x,a,a,D,E,F,a,H,x], XY, [x,XY,a,D,E,F,a,H,x]).
+moverIA([a,a,x,D,E,F,x,H,a], XY, [a,XY,x,D,E,F,x,H,a]).
+moverIA([x,x,a,D,E,F,a,a,x], XY, [x,x,a,D,E,F,a,XY,x]).
+moverIA([a,x,x,D,E,F,x,a,a], XY, [a,x,x,D,E,F,x,XY,a]).
+moverIA([A,B,C,D,E,F,G,H,a], XY, [A,B,C,D,E,F,G,H,XY]).
+moverIA([A,B,C,D,E,F,a,H,I], XY, [A,B,C,D,E,F,XY,H,I]).
+moverIA([A,B,a,D,E,F,G,H,I], XY, [A,B,XY,D,E,F,G,H,I]).
+moverIA([a,B,C,D,E,F,G,H,I], XY, [XY,B,C,D,E,F,G,H,I]).
+moverIA([A,a,C,D,E,F,G,H,I], XY, [A,XY,C,D,E,F,G,H,I]).
+moverIA([A,B,C,a,E,F,G,H,I], XY, [A,B,C,XY,E,F,G,H,I]).
+moverIA([A,B,C,D,E,a,G,H,I], XY, [A,B,C,D,E,XY,G,H,I]).
+moverIA([A,B,C,D,E,F,G,a,I], XY, [A,B,C,D,E,F,G,XY,I]).
+
+moverJ([a,B,C,D,E,F,G,H,I], 1, [x,B,C,D,E,F,G,H,I]).
+moverJ([A,a,C,D,E,F,G,H,I], 2, [A,x,C,D,E,F,G,H,I]).
+moverJ([A,B,a,D,E,F,G,H,I], 3, [A,B,x,D,E,F,G,H,I]).
+moverJ([A,B,C,a,E,F,G,H,I], 4, [A,B,C,x,E,F,G,H,I]).
+moverJ([A,B,C,D,a,F,G,H,I], 5, [A,B,C,D,x,F,G,H,I]).
+moverJ([A,B,C,D,E,a,G,H,I], 6, [A,B,C,D,E,x,G,H,I]).
+moverJ([A,B,C,D,E,F,a,H,I], 7, [A,B,C,D,E,F,x,H,I]).
+moverJ([A,B,C,D,E,F,G,a,I], 8, [A,B,C,D,E,F,G,x,I]).
+moverJ([A,B,C,D,E,F,G,H,a], 9, [A,B,C,D,E,F,G,H,x]).
+
+disp([A,B,C,D,E,F,G,H,I]) :-
+	write('|'),
+	write([A,B,C]),write('|'),nl,
+	write('|'),
+	write([D,E,F]),write('|'),nl,	write('|'),
+        write([G,H,I]),write('|'),nl,nl.
+
+main :- inicio, iniciar([a,a,a,a,a,a,a,a,a]).
+
+inicio :-
+  write('Você joga com x, digite a posição que quer jogar.'),
+  nl,
+  disp([1,2,3,4,5,6,7,8,9]).
+
+iniciar(L) :- vitoria(L, x), write('Venceu!').
+iniciar(L) :- vitoria(L, o), write('Perdeu!').
+iniciar(L) :- not(member(a,L)), write('Empatou!').
+iniciar(L) :- read(N),
+  moverJ(L, N, NL),
+  disp(NL),
+  oplay(NL, NnewL),
+  disp(NnewL),
+  iniciar(NnewL).
+
+venceJ(L) :- moverIA(L, x, NL), vitoria(NL, x).
+
+oplay(L,NL) :-
+  moverIA(L, o, NL),
+  vitoria(NL, o),!.
+oplay(L,NL) :-
+  moverIA(L, o, NL),
+  not(venceJ(NL)).
+oplay(L,NL) :-
+  moverIA(L, o, NL).
+oplay(L,NL) :-
+  not(member(a,L)),
+  NL = L.
