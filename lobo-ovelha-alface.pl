94034d6e041e3fea45aa162daaa9b70e88bdb9c4
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2018-02-06 15:57:50 -0200

    organizado e comentado
    A lógica é a mesma, mas agora está mais legível

--- lobo-ovelha-alface.pl
+++ lobo-ovelha-alface.pl
@@ -1,18 +1,25 @@
+%% Relacoes de comida independem da ordem da tupla
 come(ovelha,alface).
 come(alface,ovelha).
 come(lobo,ovelha).
 come(ovelha,lobo).
 
-atravessa([X,Y,Z], [], [Y,Z], [X], S, [X|S]):- come(Y,X), come(Z,X).
-atravessa([X,Y,Z], [], [X,Z], [Y], S, [Y|S]):- come(X,Y), come(Z,Y).
-atravessa([X,Y,Z], [], [X,Y], [Z], S, [Z|S]):- come(Y,Z), come(X,Z).
-atravessa([X,Y], [Z], [Y], [X,Z], S, [X|S]):- not(come(X,Z)).
-atravessa([X,Y], [Z], [X], [Y,Z], S, [Y|S]):- not(come(Y,Z)).
-atravessa([X,Y], [Z], [Y], [X,Z], S, [X|S]):- come(X,Z),come(Y,Z).
-atravessa([X,Y], [Z], [X], [Y,Z], S, [Y|S]):- come(Y,Z),come(X,Z).
-atravessa([X], [Y,Z], [], [X,Y,Z], S, [X|S]):- not(come(Y,Z)).
-atravessa([X], [Y,Z], [X,Z], [Y], S, [Z|S]):- come(Y,Z), come(Z,X).
-atravessa([X], [Y,Z], [X,Y], [Z], S, [Y|S]):- come(Y,Z), come(Y,X).
+%% Se a ovelha estah no lado1 com 2 acompanhantes, levamos ela.
+atravessa([ovelha,Y,Z], _, [Y,Z], [ovelha], S, [ovelha|S]).
+atravessa([X,ovelha,Z], _, [X,Z], [ovelha], S, [ovelha|S]).
+atravessa([X,Y,ovelha], _, [X,Y], [ovelha], S, [ovelha|S]).
+%% Se a ovelha estah no lado2 com 1 acompanhante, trazemos ela.
+atravessa([X], [Y,ovelha], [X,ovelha], [Y], S, [ovelha|S]).
+atravessa([X], [ovelha,Z], [X,ovelha], [Z], S, [ovelha|S]).
+%% Se a ovelha estah no lado1 com 1 acompanhante, levamos ele.
+atravessa([X,ovelha], [Z], [ovelha], [X,Z], S, [X|S]).
+atravessa([ovelha,Y], [Z], [ovelha], [Y,Z], S, [Y|S]).
+%% Se soh a ovelha estah no lado2, levamos qualquer um pra lah.
+atravessa([X,Y], [ovelha], [Y], [X,ovelha], S, [X|S]).
+atravessa([X,Y], [ovelha], [X], [Y,ovelha], S, [Y|S]).
+%% Se só sobrou a ovelha no lado1, levamos ela.
+atravessa([ovelha], [Y,Z], [], [ovelha,Y,Z], S, [ovelha|S]).
+%% A solucao eh encontrada quando o lado1 estah vazio
 
 solucao([],_,S,S).
 solucao(Lado1, Lado2, S, NSolucao) :- 
