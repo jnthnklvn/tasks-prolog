3f68c6e3c97750bf8885b7d6f77c7ae968f18db0
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2019-03-05 03:00:25 -0300

    será?

--- lobo-ovelha-alface2.pl
+++ lobo-ovelha-alface2.pl
@@ -11,11 +11,17 @@ atravessa([X,Y], [Z], [X], [Y,Z]).
 atravessa([X], [Y,Z], [], [X,Y,Z]).
 
 solucao([],_).
-solucao([E1],L2):- ((not(come(L2)), atravessa([E1], L2, L1, NL2));
-  (come(L2), atravessa(L2, [E1], NL2, L1), come(L1))), solucao(L1,NL2), write(NL2).
+%%solucao([E1],L2):- ((not(come(L2)), atravessa([E1], L2, L1, NL2));
+%%  (come(L2), atravessa(L2, [E1], NL2, L1), come(L1))), solucao(L1,NL2), write(NL2).
 
-solucao(L1,[E3]):- ((atravessa(L1, [E3], NL1, NL2), not(come(NL2)));
-  (not(come(L1)), atravessa(L1, [E3], NL1, NL2))), solucao(NL1,NL2), write(NL2).
+solucao([E1],L2):- atravessa([E1], L2, L1, NL2),
+  solucao(L1,NL2), write(NL2).
+
+solucao(L1,[E3]):- atravessa(L1, [E3], NL1, NL2), not(come(NL2)),
+  solucao(NL1,NL2), write(NL2).
+
+%%solucao(L1,[E3]):- ((atravessa(L1, [E3], NL1, NL2), not(come(NL2)));
+%%  (not(come(L1)), atravessa(L1, [E3], NL1, NL2))), solucao(NL1,NL2), write(NL2).
 
 solucao(L1,[]):- atravessa(L1, [], NL1, NL2), not(come(NL1)),
   solucao(NL1,NL2), write(NL2).
